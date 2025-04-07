5bdf581a05d859f126ecc621baf8f90c0cafe57e
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2025-04-07 23:51:29 +0545

    "add two numbers"

---
+++ StateLess_calculator_api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Numbers struct {
+	Number1 int `json:"number1"`
+	Number2 int `json:"number2"`
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/add", AddNumbers)
+	fmt.Println("Server running on localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to calculator app")
+}
+
+func AddNumbers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var numbers Numbers
+	err := json.NewDecoder(r.Body).Decode(&numbers)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	sum := numbers.Number1 + numbers.Number2
+	json.NewEncoder(w).Encode(sum)
+}
