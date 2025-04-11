75efcd926161d0e51eb371e7eacf5282c77b67eb
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2025-04-11 09:03:28 +0545

    calculator

--- StateLess_calculator_api/main.go
+++ StateLess_calculator_api/main.go
@@ -12,9 +12,16 @@ type Numbers struct {
 	Number2 int `json:"number2"`
 }
 
+type Response struct {
+	Result      int
+	Description string
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/add", AddNumbers)
+	http.HandleFunc("/subtract", SubtractNumbers)
+	http.HandleFunc("/multiply", MultiplyNumbers)
 	fmt.Println("Server running on localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
@@ -35,5 +42,63 @@ func AddNumbers(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 	sum := numbers.Number1 + numbers.Number2
-	json.NewEncoder(w).Encode(sum)
+	response := Response{
+		Result:      sum,
+		Description: "successfully added two numbers",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+func SubtractNumbers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var numbers Numbers
+	err := json.NewDecoder(r.Body).Decode(&numbers)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	sum := numbers.Number1 - numbers.Number2
+	response := Response{
+		Result:      sum,
+		Description: "successfully subtracted two numbers",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+func MultiplyNumbers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var numbers Numbers
+	err := json.NewDecoder(r.Body).Decode(&numbers)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	sum := numbers.Number1 * numbers.Number2
+	response := Response{
+		Result:      sum,
+		Description: "successfully Multiplied two numbers",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+func DivideNumbers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var numbers Numbers
+	err := json.NewDecoder(r.Body).Decode(&numbers)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	sum := numbers.Number1 * numbers.Number2
+	response := Response{
+		Result:      sum,
+		Description: "successfully Multiplied two numbers",
+	}
+	json.NewEncoder(w).Encode(response)
 }
